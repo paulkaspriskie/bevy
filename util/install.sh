8ba11f8217fb445e9580e46d4a50f21e1ecb7ac5
Paul <paulkaspriskie@gmail.com>
2024-08-21 08:18:16 -0400

    build: modified util/install.sh, created condition that adds /usr/local/bin directory to $PATH if not already added to $PATH.

--- util/install.sh
+++ util/install.sh
@@ -9,4 +9,12 @@ else
   echo "Directory '$DIR_PATH' already exists. Skipping creation..."
 fi
 
- sudo curl -o /usr/local/bin/bevy https://raw.githubusercontent.com/paulkaspriskie/bevy/main/bevy && sudo chmod +x /usr/local/bin/bevy
+
+if ! echo $PATH | grep -q ":$DIR_PATH:"; then
+  export PATH="$PATH:$DIR_PATH"
+  echo "Directory '$DIR_PATH' added to your PATH."
+else
+  echo "Directory '$DIR_PATH' is already in your PATH. Skipping addition..."
+fi
+
+sudo curl -o /usr/local/bin/bevy https://raw.githubusercontent.com/paulkaspriskie/bevy/main/bevy && sudo chmod +x /usr/local/bin/bevy
