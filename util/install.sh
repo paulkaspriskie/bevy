9f0469e60a84d8a5f59b740e697f6a9a36e46d7b
Paul <paulkaspriskie@gmail.com>
2024-08-21 08:04:31 -0400

    build: modified util/install.sh, added condition that creates /usr/local/bin directory if one does not exists.

--- util/install.sh
+++ util/install.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
+DIR_PATH=/usr/local/bin
+
+if [ ! -d "$DIR_PATH" ]; then
+  mkdir -p $DIR_PATH
+  echo "Directory '$DIR_PATH' created successfully"
+else
+  echo "Directory '$DIR_PATH' already exists. Skipping creation..."
+fi
+
  sudo curl -o /usr/local/bin/bevy https://raw.githubusercontent.com/paulkaspriskie/bevy/main/bevy && sudo chmod +x /usr/local/bin/bevy
